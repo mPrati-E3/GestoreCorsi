121c3e7ce8f7d4850ee77607fc7cf9bd7f35ecde
MPrati <MPrati@HP-250-G8-MP.homenet.telecomitalia.it>
2021-03-29 16:27:42 +0200

    Generazione java bean + impostazione DAO

--- src/main/java/it/polito/tdp/corsi/model/Corso.java
+++ src/main/java/it/polito/tdp/corsi/model/Corso.java
@@ -1,5 +1,33 @@
 package it.polito.tdp.corsi.model;
 
 public class Corso {
+	
+	String Nome;
+	int Iscritti;
+	
+	public Corso(String nome, int iscritti) {
+		super();
+		Nome = nome;
+		Iscritti = iscritti;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public int getIscritti() {
+		return Iscritti;
+	}
+
+	public void setIscritti(int iscritti) {
+		Iscritti = iscritti;
+	}
+	
+	
+	
 
 }
--- src/main/java/it/polito/tdp/corsi/model/Studente.java
+++ src/main/java/it/polito/tdp/corsi/model/Studente.java
@@ -1,5 +1,41 @@
 package it.polito.tdp.corsi.model;
 
 public class Studente {
+	
+	String Nome;
+	String Cognome;
+	String CDS;
+	
+	public Studente(String nome, String cognome, String cDS) {
+		super();
+		Nome = nome;
+		Cognome = cognome;
+		CDS = cDS;
+	}
 
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getCognome() {
+		return Cognome;
+	}
+
+	public void setCognome(String cognome) {
+		Cognome = cognome;
+	}
+
+	public String getCDS() {
+		return CDS;
+	}
+
+	public void setCDS(String cDS) {
+		CDS = cDS;
+	}
+	
+	
 }
